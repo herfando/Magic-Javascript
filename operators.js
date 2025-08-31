530d37046b12c92a0e04954e087dde43557928c6
Herfando <herfando_91@yahoo.com>
2025-08-31 18:07:56 +0700

    make operators.js

---
+++ operators.js
@@ -0,0 +1,9 @@
+let a = 10;
+let b = 5;
+
+console.log(a + b);        // Addition
+console.log(a - b);        // Subtraction
+console.log(a * b);        // Multiplication
+console.log(a / b);        // Division
+console.log(a % b);        // Modulo (remainder)
+console.log(a ** b);       // Exponentiation
