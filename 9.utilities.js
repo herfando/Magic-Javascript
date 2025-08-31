170168ef765b1e1ff47571a242ba2c5f6c4fe2f6
Herfando <herfando_91@yahoo.com>
2025-08-31 19:57:43 +0700

    create utilities.js

---
+++ 9.utilities.js
@@ -0,0 +1,3 @@
+export function multiply(x, y) { return x * y; }   // multiply two numbers
+export function isEven(number) { return number % 2 === 0; } // check even
+export function greet(name) { return "Hello, " + name; }   // greeting
